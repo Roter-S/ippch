ad3a75668769bd182eaea656ea9f780f090f352c
Roter-S <bryan.sosof@gmail.com>
2023-10-05 10:04:07 -0600

    Fix: "private layout" component decomposition.
    Coponents: Navbar, MUIDrawer.

--- src/components/layouts/PrivateLayout.tsx
+++ src/components/layouts/PrivateLayout.tsx
@@ -2,17 +2,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useUserContext } from "../../context/UserContext";
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import Toolbar from "@mui/material/Toolbar";
 import Navbar from "../../components/Common/Navbar";
+import MuiDrawer from "../Common/MUIDrawer";
 
 const drawerWidth = 240;
 
@@ -30,36 +22,6 @@ const Private: React.FC<Props> = (props) => {
     setMobileOpen(!mobileOpen);
   };
 
-  const drawer = (
-    <div>
-      <Toolbar>Logo</Toolbar>
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
@@ -74,38 +36,12 @@ const Private: React.FC<Props> = (props) => {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        <Drawer
+        <MuiDrawer
+          mobileOpen={mobileOpen}
+          handleDrawerToggle={handleDrawerToggle}
+          drawerWidth={drawerWidth}
           container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              background: "transparent",
-              borderRight: "1px solid #232E4F",
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
+        />
       </Box>
       <Box
         component="main"
