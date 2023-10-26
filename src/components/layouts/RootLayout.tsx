0963a4511af015f42b4e33dba811f2ff2347dfb1
Roter-S <bryan.sosof@gmail.com>
2023-10-26 15:21:59 -0600

    Fix: refactor eslint

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,12 +1,11 @@
-import { Outlet } from "react-router-dom";
-import UserContextProvider from "../../context/UserContext";
+import { Outlet } from 'react-router-dom'
+import UserContextProvider from '../../context/UserContext'
 
 const Root: React.FC = () => {
   return (
     <UserContextProvider>
       <Outlet />
     </UserContextProvider>
-  );
-};
-
-export default Root;
+  )
+}
+export default Root
