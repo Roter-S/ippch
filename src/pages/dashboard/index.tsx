4f7c4b29aafbfbc2c6dd2122e249717241b81830
Roter-S <bryan.sosof@gmail.com>
2023-10-26 15:13:10 -0600

    Fix: grid 2 to grid

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,7 @@
-import Grid from '@mui/material/Unstable_Grid2'
+import Grid from '@mui/material/Grid'
 import ChartPie from './ChartPie'
 import ChartBar from './ChartBar'
 import ChartArea from './ChartArea'
-import Item from '../../components/common/Grid/Item'
 import UserCard from './UserCard'
 import AttendanceCard from './AttendanceCard'
 import FamilyCard from './FamilyCard'
@@ -11,46 +10,34 @@ import CellCard from './CellCard'
 const Dashboard = () => {
   return (
     <Grid container spacing={3}>
-      <Grid xs={12}>
-        <Item>
-          <Grid container spacing={3}>
-            <Grid lg={4} md={6} sm={6} xs={12}>
-              <Item>
-                <UserCard />
-              </Item>
-            </Grid>
-            <Grid lg={4} md={6} sm={6} xs={12}>
-              <Item>
-                <AttendanceCard />
-              </Item>
-            </Grid>
-            <Grid lg={4} md={12} sm={12} xs={12}>
-              <Item>
-                <Grid container spacing={1}>
-                  <Grid sm={6} xs={12} md={6} lg={12}>
-                    <Item>
-                      <FamilyCard />
-                    </Item>
-                  </Grid>
-                  <Grid sm={6} xs={12} md={6} lg={12}>
-                    <Item>
-                      <CellCard />
-                    </Item>
-                  </Grid>
-                </Grid>
-              </Item>
-            </Grid>
-            <Grid lg={6} xs={12}>
-              <ChartBar />
-            </Grid>
-            <Grid lg={3} xs={12}>
-              <ChartPie />
-            </Grid>
-            <Grid lg={3} xs={12}>
-              <ChartArea />
+      <Grid item xs={12}>
+        <Grid container spacing={3}>
+          <Grid item lg={4} md={6} sm={6} xs={12}>
+            <UserCard />
+          </Grid>
+          <Grid item lg={4} md={6} sm={6} xs={12}>
+            <AttendanceCard />
+          </Grid>
+          <Grid item lg={4} md={12} sm={12} xs={12}>
+            <Grid container spacing={1}>
+              <Grid item sm={6} xs={12} md={6} lg={12}>
+                <FamilyCard />
+              </Grid>
+              <Grid item sm={6} xs={12} md={6} lg={12}>
+                <CellCard />
+              </Grid>
             </Grid>
           </Grid>
-        </Item>
+          <Grid item lg={6} xs={12}>
+            <ChartBar />
+          </Grid>
+          <Grid item lg={3} xs={12}>
+            <ChartPie />
+          </Grid>
+          <Grid item lg={3} xs={12}>
+            <ChartArea />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   )
