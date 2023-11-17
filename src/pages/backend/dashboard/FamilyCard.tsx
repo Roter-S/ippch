28345b89b76f170bca154a799c41cc03f6a8a163
Roter-S <bryan.sosof@gmail.com>
2023-11-16 19:29:45 -0600

    Fix: dashboard

--- src/pages/backend/dashboard/FamilyCard.tsx
+++ src/pages/backend/dashboard/FamilyCard.tsx
@@ -1,15 +1,35 @@
 import DashboardTwo from '../../../components/common/cards/DashboardTwo'
 import FactCheckIcon from '@mui/icons-material/FactCheck'
 import { useTheme } from '@mui/material/styles'
+import { useEffect, useState } from 'react'
+import { getDocumentCount } from '../../../utils/firestoreUtils'
+import { useAlert } from '../../../context/AlertContext'
 
 const FamilyCard = () => {
   const theme = useTheme()
+  const showAlert = useAlert()
+  const [loading, setLoading] = useState<boolean>(true)
+  const [GroupCount, setGroupCount] = useState(0)
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const users = await getDocumentCount('groups')
+        setGroupCount(users)
+        setLoading(false)
+      } catch (error: Error | any) {
+        showAlert(error.message, 'error')
+      }
+    }
+    void fetchUsers()
+  }
+  , [])
 
   return <DashboardTwo
-            isLoading={false}
+            isLoading={loading}
             icon={<FactCheckIcon sx={{ fontSize: 40 }}/>}
-            value={250}
-            title='Asistencia'
+            value={GroupCount}
+            title='Total de grupos'
             backgroundColor={theme.palette.violet.main}
             background={theme.palette.violet.dark}
             />
